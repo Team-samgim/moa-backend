59df5f068bbefc23a75658199a32208778a73cac
daehyuk1231 <daehyuk1231@naver.com>
2025-10-27 16:59:30 +0900

    feat: 정렬 구현

--- src/main/java/com/moa/global/security/SecurityConfig.java
+++ src/main/java/com/moa/global/security/SecurityConfig.java
@@ -39,8 +39,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                     // 회원 관련 api (api/auth/로 시작하는 api)는 토큰 없이 접근 가능
                     .requestMatchers("/api/auth/**").permitAll()
-                    .requestMatchers("/api/mock-search").permitAll()
-//                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
