e9faabc424d390f269c5088179ac1871135e2218
정소영 <so2043@naver.com>
2025-11-04 14:11:29 +0900

    feat : 정보성 필드 여부, 필드 테이블 공용 entity, repository 생성

--- src/main/java/com/moa/api/search/dto/FieldWithOpsDTO.java
+++ src/main/java/com/moa/api/search/dto/FieldWithOpsDTO.java
@@ -1,16 +1,23 @@
 package com.moa.api.search.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonPropertyOrder({"key", "label", "labelKo", "dataType", "isInfo", "orderNo", "operators"})
 public class FieldWithOpsDTO {
-    private String key;        // 예: "src_ip"
-    private String label;      // 예: "src_ip"
-    private String dataType;   // 예: "IP"
-    private Integer orderNo;   // 정렬용(옵션)
-    private List<OperatorDTO> operators; // 이 필드에서 쓸 수 있는 연산자들
+    private String key;
+    private String label;
+    private String labelKo;
+    private String dataType;
+    private boolean isInfo;
+    private Integer orderNo;
+    private List<OperatorDTO> operators;
 }
