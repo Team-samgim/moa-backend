f0b8c62e08e600ac927daa13480d3c449a4eddd9
최이서 <chtoqur@gmail.com>
2025-11-05 17:07:13 +0900

    [SGK-208] feat: 피벗 테이블 무한 스크롤 API 추가

---
+++ src/main/java/com/moa/api/pivot/dto/RowGroupItemsRequestDTO.java
@@ -0,0 +1,25 @@
+package com.moa.api.pivot.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+public class RowGroupItemsRequestDTO {
+
+    private String layer;
+
+    private String rowField; // ex: "dst_mac"
+
+    private PivotQueryRequestDTO.TimeDef time;
+    private PivotQueryRequestDTO.ColumnDef column;
+    private List<PivotQueryRequestDTO.ValueDef> values;
+    private List<PivotQueryRequestDTO.FilterDef> filters;
+
+    private String cursor;      // "offset:100" 또는 "lastValue:abc"
+    private Integer limit = 50; // 기본 50개
+}
